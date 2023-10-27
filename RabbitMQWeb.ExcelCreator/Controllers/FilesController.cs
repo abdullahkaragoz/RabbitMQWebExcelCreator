68d78bf0b8773b10a1966db234ab676b34a0ce3f
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2023-10-27 17:15:55 +0300

    FilesController

---
+++ RabbitMQWeb.ExcelCreator/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.ExcelCreator.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.ExcelCreator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FilesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Updoad(FormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+            var userFile = await _context.UserFiles.FirstAsync(x=>x.Id == fileId);
+            var filePath  = userFile.FileName + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/files", file.FileName);
+
+            using FileStream stream = new(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _context.SaveChangesAsync();
+
+
+            return Ok();
+        }
+    }
+}
